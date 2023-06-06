69b1087c8d65bc37d48498c610c27681357722e1
pabloperotti <pabloperotti@icloud.com>
2023-06-05 22:27:18 -0300

    casi terminado

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,7 @@
 <?php
-$url_base = "http://localhost/app/";
-?>
+$url_base = "http://localhost/app/"
+    ?>
+
 <!doctype html>
 <html lang="en">
 
@@ -14,6 +15,12 @@ $url_base = "http://localhost/app/";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
+
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
 </head>
 
 <body>
@@ -21,25 +28,23 @@ $url_base = "http://localhost/app/";
         <nav class="navbar navbar-expand navbar-light bg-light">
             <ul class="nav navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" href="<?php echo $url_base ?>" aria-current="page">Sistema<span
+                    <a class="nav-link active" href="#" aria-current="page">Sistema <span
                             class="visually-hidden">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?php echo $url_base ?>secciones/empleados">Empleados</a>
                 </li>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?php echo $url_base ?>secciones/puestos">Puestos</a>
-                </li>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?php echo $url_base ?>secciones/usuarios">Usuarios</a>
                 </li>
-                </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="">Cerrar Sesion</a>
+                    <a class="nav-link" href="#">Cerrar Sesion</a>
                 </li>
             </ul>
         </nav>
     </header>
     <main class="container">
+        <br>
